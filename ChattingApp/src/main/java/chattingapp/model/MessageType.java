cd1fe35d9e4e66b6d7727ba857f974e2ee19ff5f
Dao Cong Pha <mongpha90@gmail.com>
2026-02-12 07:43:02 +0700

    New Save

--- ChattingApp/src/main/java/chattingapp/model/MessageType.java
+++ ChattingApp/src/main/java/chattingapp/model/MessageType.java
@@ -4,16 +4,14 @@ package chattingapp.model;
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 /**
  *
  * @author CP
  */
 public enum MessageType {
-        TEXT,
-        IMAGE,
-        FILE,
-        VIDEO,
-        AUDIO
-    }
-
+    TEXT,
+    IMAGE,
+    FILE,
+    VIDEO,
+    AUDIO
+}
--- ChattingApp/src/main/java/chattingapp/model/NotificationType.java
+++ ChattingApp/src/main/java/chattingapp/model/NotificationType.java
@@ -10,5 +10,5 @@ package chattingapp.model;
  */
 public enum NotificationType {
     FRIEND,
-MESSAGE,
+    MESSAGE,
 }

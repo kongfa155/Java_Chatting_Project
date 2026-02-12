51bdc055dd1bd06ac4ab68678e6228861caf2508
nhhun2005 <nhhun2005@gmail.com>
2026-02-12 07:25:44 +0700

    chore: split backend to seperated package

---
+++ chatting-app-backend/src/main/java/com/mycompany/chattingapp/chattingappbackend/ChattingAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mycompany.chattingapp.chattingappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChattingAppBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChattingAppBackendApplication.class, args);
+    }
+
+}

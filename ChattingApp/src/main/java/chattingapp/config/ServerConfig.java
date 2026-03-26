9b19a6caed59f5449fd663c59bf3fd166d1302bc
nhhun2005 <nhhun2005@gmail.com>
2026-03-26 11:20:05 +0700

    chinh sua ngoc trinh

---
+++ ChattingApp/src/main/java/chattingapp/config/ServerConfig.java
@@ -0,0 +1,14 @@
+package chattingapp.config;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author nhhun2005
+ */
+public class ServerConfig {
+    public static final String SERVER_URL="http://192.168.1.131:8080";
+}
